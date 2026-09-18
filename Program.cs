b17bf06b70aab8235dbb51d7905daeea1e428af5
mazensaadany <mazenelsaadany3@gmail.com>
2026-09-18 17:18:51 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RouteAss_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
